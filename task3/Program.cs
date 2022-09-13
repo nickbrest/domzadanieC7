4a79512589ecf2165de677237a7529153beb57f7
Nick <mikabrmail@gmail.com>
2022-09-14 02:38:43 +0300

    task3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] CreateMatrix(int length, int width, int minimum, int maximum)
+{
+    int[,] matrix = new int[length, width];
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = random.Next(minimum, maximum);
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+
+int[,] ChangeFirstLastString(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+    return matrix;
+}
+
+int[,] matrix = CreateMatrix(5, 7, 0, 20);
+PrintMatrix(matrix);
+Console.WriteLine();
+ChangeFirstLastString(matrix);
+PrintMatrix(matrix);
